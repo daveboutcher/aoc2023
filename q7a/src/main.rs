27b4aa08ea178401c7b70381f777ea2b2429ccf6
Dave Boutcher <daveboutcher@gmail.com>
2023-12-07 10:11:47 +0000

    a little code cleanup

--- q7a/src/main.rs
+++ q7a/src/main.rs
@@ -12,6 +12,7 @@ fn read_input() -> Vec<String> {
 fn solve() -> usize {
     read_input()
         .iter()
+        // Update the face card names to sort correctly
         .map(|line| {
             line.replace("A", "Z")
                 .replace("K", "Y")
@@ -19,18 +20,23 @@ fn solve() -> usize {
                 .replace("J", "W")
                 .replace("T", "V")
         })
+        // Break the line into the hand and the bid
         .map(|line| (line[0..5].to_string(), line[6..].parse::<usize>().unwrap()))
+        // Count the most frequently occurring card
         .map(|(hand, bid)| {
             (
-                hand.clone(),
-                bid,
                 hand.chars()
                     .map(|c| (hand.chars().filter(|c1| c == *c1).count(), c))
                     .max()
                     .unwrap(),
+                hand,
+                bid,
             )
         })
-        .map(|(hand, bid, max1)| {
+        // Count the second most frequently occurring card and produce a score
+        // so 5 of a kind will be 50, 3 of a kind will be 30, full house will be
+        // 32, two pair will be 22
+        .map(|(max1, hand, bid)| {
             (
                 (max1.0 * 10
                     + hand
@@ -42,11 +48,13 @@ fn solve() -> usize {
                         })
                         .max()
                         .unwrap()),
-                hand.clone(),
+                hand,
                 bid,
             )
         })
+        // Put it in a binary heap so it sorts by score then hand
         .collect::<BinaryHeap<_>>()
+        // Note that BinaryYeap.iter() does NOT do what you would expect...
         .into_sorted_vec()
         .iter()
         .enumerate()
--- q7b/src/main.rs
+++ q7b/src/main.rs
@@ -12,6 +12,7 @@ fn read_input() -> Vec<String> {
 fn solve() -> usize {
     read_input()
         .iter()
+        // Update the face card names to sort correctly. Joker now sorts lowest
         .map(|line| {
             line.replace("A", "Z")
                 .replace("K", "Y")
@@ -19,21 +20,23 @@ fn solve() -> usize {
                 .replace("J", "0")
                 .replace("T", "V")
         })
+        // Break the line into the hand and the bid
         .map(|line| (line[0..5].to_string(), line[6..].parse::<usize>().unwrap()))
+        // Count the most frequently occurring card
         .map(|(hand, bid)| {
             (
-                hand.clone(),
-                bid,
                 hand.chars()
                     .map(|c| (hand.chars().filter(|c1| c == *c1).count(), c))
                     .max()
                     .unwrap(),
-            )
-        })
-        .map(|(hand, bid, max1)| {
-            (
-                hand.clone(),
+                hand,
                 bid,
+    
+                )
+        })
+        // count the second most frequently occurring card
+        .map(|(max1, hand, bid)| {
+            (
                 max1,
                 (hand
                     .chars()
@@ -47,31 +50,41 @@ fn solve() -> usize {
                     })
                     .max()
                     .unwrap()),
-            )
+                    hand,
+                    bid,
+                )
         })
-        .map(|(hand, bid, max1, max2)| {
+        // In a copy of the hand (hand2), replace the joker with the most
+        // frequently occurring other card.  The subtle thing here is that
+        // if the joker is the most frequently occuring card, we need to 
+        // change jokers to the second most frequently occuring card 
+        .map(|(max1, max2, hand, bid)| {
             (
-                hand.clone(),
                 match max1.1 {
                     '0' => hand.replace("0", &max2.1.to_string()),
                     c => hand.replace("0", &c.to_string()),
                 },
+                hand,
                 bid,
             )
         })
-        .map(|(hand, hand2, bid)| {
+        // Now recalculate the most frequently occurring card using hand2
+        .map(|(hand2, hand, bid)| {
             (
-                hand.clone(),
-                hand2.clone(),
-                bid,
                 hand2
                     .chars()
                     .map(|c| (hand2.chars().filter(|c1| c == *c1).count(), c))
                     .max()
                     .unwrap(),
-            )
+                    hand,
+                    hand2,
+                    bid,
+                )
         })
-        .map(|(hand, hand2, bid, max1)| {
+        // Count the second most frequently occurring card using hand2 and produce a score
+        // so 5 of a kind will be 50, 3 of a kind will be 30, full house will be
+        // 32, two pair will be 22
+        .map(|(max1, hand, hand2, bid)| {
             (
                 max1.0 * 10
                     + hand2
@@ -84,11 +97,13 @@ fn solve() -> usize {
                         })
                         .max()
                         .unwrap(),
-                hand.clone(),
+                hand,
                 bid,
             )
         })
+        // Put it in a binary heap so it sorts by score then hand
         .collect::<BinaryHeap<_>>()
+        // Note that BinaryYeap.iter() does NOT do what you would expect...
         .into_sorted_vec()
         .iter()
         .enumerate()
